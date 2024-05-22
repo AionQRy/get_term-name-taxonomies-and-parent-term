d3fdc1de1ccadcdaff6f0211637eed565ce05384
Akegapol Pinyopun <anacuse15@hotmail.com>
2024-05-23 04:33:51 +0700

    Update get_term.php

--- get_term.php
+++ get_term.php
@@ -2,7 +2,33 @@
 // Fetch all terms from the 'project_categories' taxonomy
 $terms = get_terms(array(
     'taxonomy' => 'project_categories',
-    'hide_empty' => false, // Set to true if you want to hide empty categories
+    'hide_empty' => false, // Show even empty categories
+    'include' => array(1, 2), // Include specific term IDs
+    'exclude' => array(3, 4), // Exclude specific term IDs
+    'number' => 5, // Limit the number of terms retrieved
+    'offset' => 10, // Skip the first N terms
+    'fields' => 'all', // What fields to return ('ids', 'all')
+    'orderby' => 'name', // Order by field ('id', 'name', etc.)
+    'order' => 'ASC', // Order type ('ASC', 'DESC')
+    'slug' => 'example-slug', // Slug to match exactly
+    'search' => '', // Search term(s)
+    'hierarchical' => true, // Whether to return hierarchical terms
+    'child_of' => 0, // Only children of this term
+    'parent' => 0, // Only direct parents of this term
+    'pad_counts' => false, // Pad counts of child terms
+    'echo' => false, // Whether to echo terms
+    'feed' => '', // Feed link prefix
+    'feed_type' => '', // Feed type, if 'feed'
+    'feed_class' => '', // CSS class used for enclosing feed link
+    'feed_image' => '', // URL of feed image to show for syndication links
+    'link' => '', // HTML link wrapped around term
+    'title_li' => '', // List items title attribute
+    'walker' => '', // Instance of Walker_Term to walk the tree
+    'separator' => ', ', // Terms are joined by this string
+    'popular_terms' => false, // Whether to retrieve popular terms
+    'tax_query' => array(), // Taxonomy query parameters
+    'update_post_meta_cache' => true, // Update post meta cache
+    'update_term_meta_cache' => true, // Update term meta cache
 ));
 
 // Check if any terms were returned
